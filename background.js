f0a939609b330e8f1eb1eee1d9829d046be1168f
Deliaz <D3liaz@gmail.com>
2018-08-30 18:18:06 +0700

    Improve logic for pausing youtube

--- background.js
+++ background.js
@@ -17,22 +17,33 @@ const patterns = [
 chrome.browserAction.onClicked.addListener(function () {
 
 	chrome.tabs.query({currentWindow: true}, tabs => {
+		const audibleCount = tabs.filter(tab => tab.audible).length;
+
 		tabs.forEach(tab => {
 			patterns.forEach(pattern => {
 				if (pattern.url.test(tab.url)) {
-					if (!pattern.inject) {
-						chrome.tabs.executeScript(tab.id, {
-							code: pattern.code
-						});
-					} else {
-						chrome.tabs.sendRequest(tab.id, {
-							action: 'playpause',
-							eventName: pattern.eventName
-						});
-					}
+					applyRule(tab, pattern, audibleCount);
 				}
 			})
 		});
 	});
 
 });
+
+function applyRule(tab, pattern, audibleCount) {
+	if (tab.url.includes('www.youtube.com/') && tab.active && audibleCount && !(tab.audible && audibleCount === 1)) {
+		// Skip pausing youtube video in case if we have any players in other tabs
+		return;
+	}
+
+	if (!pattern.inject) {
+		chrome.tabs.executeScript(tab.id, {
+			code: pattern.code
+		});
+	} else {
+		chrome.tabs.sendRequest(tab.id, {
+			action: 'playpause',
+			eventName: pattern.eventName
+		});
+	}
+}
